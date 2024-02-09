c14613a78ce94a71ce502174634ff2b183234f82
Roberto Moura <roblm_@hotmail.com>
2024-02-09 19:52:02 -0300

    Add project files.

---
+++ 001_TwoSum/Program.cs
@@ -0,0 +1,34 @@
+﻿static int[] TwoSum(int[] nums, int target)
+{
+    int[] result = new int[2];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = 0; j < nums.Length; j++)
+        {
+            if (i != j)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    result[0] = i;
+                    result[1] = j;
+                    Console.WriteLine(result[0] + ", " + result[1]);
+                    return result;
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+int[] nums = { 2, 7, 11, 15 };
+int target = 9;
+
+//int[] nums = { 3, 2, 4 };
+//int target = 6;
+
+//int[] nums = { 3, 3 };
+//int target = 6;
+
+TwoSum(nums, target);
