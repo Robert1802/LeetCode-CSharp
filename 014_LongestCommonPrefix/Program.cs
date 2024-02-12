874f72a46160b5331df284037aeb85069facfa5a
Roberto Moura <roblm_@hotmail.com>
2024-02-12 20:41:00 -0300

    014_LongestCommonPrefix

---
+++ 014_LongestCommonPrefix/Program.cs
@@ -0,0 +1,52 @@
+﻿// https://leetcode.com/problems/longest-common-prefix/description/
+
+/*
+ Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+ */
+
+
+static string LongestCommonPrefix(string[] strs)
+{
+	string currentPrefix = "";
+    var sorted = strs.OrderBy(n => n.Length);
+    var shortest = sorted.FirstOrDefault();
+    if (shortest.Length == 1)
+    {
+        int divisor = strs.Count(p => p.Substring(0, 1) != strs[0].Substring(0,1));
+        if (divisor > 0)
+        {
+            return "";
+        }
+        return shortest;
+    }
+
+    for (int i = 0; i <= shortest.Length - 1; i++)
+    {
+        currentPrefix = strs[0].Substring(0, 1 + i);
+        for (int j = 0; j < strs.Length; j++)
+        {
+            Console.WriteLine(strs[j].Substring(0,1 + i));
+            if (currentPrefix != strs[j].Substring(0, 1 + i))
+                return strs[j].Substring(0,i);
+        }
+    }
+    return currentPrefix;
+}
+
+string[] strs = { "flower", "flow", "flight" };
+Console.WriteLine(LongestCommonPrefix(strs));
--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Vou deixar o link de cada teste e do meu codigo nessa pagina
 - 001 - Two Sum - [LeetCode](https://leetcode.com/problems/two-sum/) - [Solucao](https://github.com/Robert1802/LeetCode-CSharp/blob/master/001_TwoSum/Program.cs)
 - 009 - Palindrome Number - [LeetCode](https://leetcode.com/problems/palindrome-number/) - [Solucao](https://github.com/Robert1802/LeetCode-CSharp/blob/master/009_PalindromeNumber/Program.cs)
 - 013 - Roman To Integrer - [LeetCode](https://leetcode.com/problems/roman-to-integer/) - [Solucao](https://github.com/Robert1802/LeetCode-CSharp/blob/master/013_RomanToInt/Program.cs)
+- 014 - Longest Common Prefix - [LeetCode](https://leetcode.com/problems/longest-common-prefix/) - [Solucao](https://github.com/Robert1802/LeetCode-CSharp/blob/master/014_LongestCommonPrefix/Program.cs)
