eab66bd32f67f7f75b91e8f56dbc526f965e1218
Roberto Moura <roblm_@hotmail.com>
2024-02-21 17:41:42 -0300

    20 Valid Parenteses

---
+++ 020_ValidParentheses/Program.cs
@@ -0,0 +1,67 @@
+﻿// https://leetcode.com/problems/valid-parentheses/description/
+
+/*
+ Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+
+ */
+
+
+static bool IsValid(string s)
+{
+    while (s.Contains("()") || s.Contains("{}") || s.Contains("[]"))
+    {
+        if (s.Contains("()"))
+        {
+            s = s.Replace("()", "");
+        }
+
+        if (s.Contains("{}"))
+        {
+            s = s.Replace("{}", "");
+        }
+
+        if (s.Contains("[]"))
+        {
+            s = s.Replace("[]", "");
+        }
+    }
+    if (s.Length == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+string input = "()";
+Console.WriteLine(IsValid(input)); //true
+
+input = "()[]{}";
+Console.WriteLine(IsValid(input)); //true
+
+input = "(]";
+Console.WriteLine(IsValid(input)); //false
+
+input = "{[]}";
+Console.WriteLine(IsValid(input)); //true
--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Vou deixar o link de cada teste e do meu codigo nessa pagina
 - 009 - Palindrome Number - [LeetCode](https://leetcode.com/problems/palindrome-number/) - [Solucao](https://github.com/Robert1802/LeetCode-CSharp/blob/master/009_PalindromeNumber/Program.cs)
 - 013 - Roman To Integrer - [LeetCode](https://leetcode.com/problems/roman-to-integer/) - [Solucao](https://github.com/Robert1802/LeetCode-CSharp/blob/master/013_RomanToInt/Program.cs)
 - 014 - Longest Common Prefix - [LeetCode](https://leetcode.com/problems/longest-common-prefix/) - [Solucao](https://github.com/Robert1802/LeetCode-CSharp/blob/master/014_LongestCommonPrefix/Program.cs)
+- 020 - Valid Parentheses - [LeetCode](https://leetcode.com/problems/valid-parentheses/) - [Solucao](https://github.com/Robert1802/LeetCode-CSharp/blob/master/020_ValidParentheses/Program.cs)
